6b193f3df296314b9876acb282433d6b641852d2
dan-dean <78292223+dan-dean@users.noreply.github.com>
2023-10-30 19:00:40 -0600

    added comment

--- input.py
+++ input.py
@@ -1,4 +1,7 @@
 # Python script to load input data and text embedder model and index into the database.
+
+# place input files in inputs/ and jina-embeddings-v2-base-en directory in models/
+
 from transformers import AutoTokenizer, AutoModel
 
 model_dir = "./jina-embeddings-v2-base-en"
